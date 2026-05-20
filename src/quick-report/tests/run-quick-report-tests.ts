43a9a2ea79a49c601364a676607d91f3e89e0fc9
braulinopeixoto-tech <braulinopeixoto@gmail.com>
2026-05-20 08:52:55 -0300

    Run raw parser regression tests

--- src/quick-report/tests/run-quick-report-tests.ts
+++ src/quick-report/tests/run-quick-report-tests.ts
@@ -1,9 +1,11 @@
 import { runQuickReportProfileTests } from './quick-report-profiles.test'
 import { runQuickReportProductionReadinessTest } from './quick-report-production-readiness.test'
+import { runQuickReportRawParserTests } from './quick-report-raw-parser.test'
 import { runQuickReportSmokeTests } from './quick-report.test'
 
 runQuickReportSmokeTests()
 runQuickReportProfileTests()
 runQuickReportProductionReadinessTest()
+runQuickReportRawParserTests()
 
 console.log('Quick Report checks passed.')
