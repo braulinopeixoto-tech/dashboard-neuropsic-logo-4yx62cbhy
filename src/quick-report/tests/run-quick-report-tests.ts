122d6fe59bcf222e0eae192e2d7dcc8b15b4039e
braulinopeixoto-tech <braulinopeixoto@gmail.com>
2026-05-20 10:53:38 -0300

    Run Quick Report UI adapter tests

--- src/quick-report/tests/run-quick-report-tests.ts
+++ src/quick-report/tests/run-quick-report-tests.ts
@@ -2,10 +2,12 @@ import { runQuickReportProfileTests } from './quick-report-profiles.test'
 import { runQuickReportProductionReadinessTest } from './quick-report-production-readiness.test'
 import { runQuickReportRawParserTests } from './quick-report-raw-parser.test'
 import { runQuickReportSmokeTests } from './quick-report.test'
+import { runQuickReportUiAdapterTests } from './quick-report-ui-adapter.test'
 
 runQuickReportSmokeTests()
 runQuickReportProfileTests()
 runQuickReportProductionReadinessTest()
 runQuickReportRawParserTests()
+runQuickReportUiAdapterTests()
 
 console.log('Quick Report checks passed.')
